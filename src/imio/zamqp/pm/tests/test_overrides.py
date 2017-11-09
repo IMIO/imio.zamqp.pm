be848afef1b4366def904ad1bd5957236f39b813
Gauthier Bastien <g.bastien@imio.be>
2017-11-09 12:17:27 +0100

    Added test_overrides.py

---
+++ src/imio/zamqp/pm/tests/test_overrides.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+from imio.zamqp.pm.tests.base import BaseTestCase
+
+
+class TestOverrides(BaseTestCase):
+
+    def test_pod_template_generation_context(self):
+        """When generating a document from a POD template,
+           the generation context contains 'zamqp_utils'."""
+        self.changeUser('pmCreator1')
+        item = self.create('MeetingItem')
+        view = item.restrictedTraverse('@@document-generation')
+        # zamqp_utils
+        self.assertTrue('zamqp_utils' in view.get_base_generation_context())
+        # in addition to values added by PloneMeeting
+        self.assertTrue('tool' in view.get_base_generation_context())
+        self.assertTrue('meetingConfig' in view.get_base_generation_context())
