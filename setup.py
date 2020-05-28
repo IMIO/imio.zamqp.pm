3820a53acea9ff15da2de3c64549c5e8ce1cfa7f
Gauthier Bastien <g.bastien@imio.be>
2020-05-28 15:05:50 +0200

    Preparing release 0.10

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-version = '0.10.dev0'
+version = '0.10'
 
 long_description = (
     open('README.rst').read() + '\n\n' + open('CHANGES.rst').read() + '\n')
