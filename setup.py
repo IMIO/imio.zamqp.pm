c7644421ce80e98cca20c3f930eb7772db95df0d
Gauthier Bastien <gauthier.bastien@imio.be>
2022-01-07 09:22:11 +0100

    Back to development: 0.15

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-version = '0.14'
+version = '0.15.dev0'
 
 long_description = (
     open('README.rst').read() + '\n\n' + open('CHANGES.rst').read() + '\n')
